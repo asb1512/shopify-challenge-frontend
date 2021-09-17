1061cf44eacd7934f71568d85d96aed2c5a760a1
asb1512 <andrew@olympicmedia.com>
2021-09-17 02:16:45 -0400

    added rows and columns to ImageGroup

--- src/components/ImageGroup.js
+++ src/components/ImageGroup.js
@@ -1,5 +1,14 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function ImageGroup() {
   return (
-    <div></div>
+    <>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </>
   )
 }
