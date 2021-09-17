a9352d6210c39ef93b929f4a7dab55aa3ed3ce13
asb1512 <andrew@olympicmedia.com>
2021-09-17 15:26:22 -0400

    setting up ImageCard's display data

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,17 +1,19 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-export default function ImageCard() {
+export default function ImageCard(props) {
+
+  console.log("Image card data", props.image)
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={props.image.img_src} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{props.image.rover.name} Rover</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
+          Some quick example text to build on the card title and make up the bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Like</Button>
       </Card.Body>
     </Card>
   )
