e7fd5c72830eb40b696383f846c9c1972ca8e531
asb1512 <andrew@olympicmedia.com>
2021-09-17 02:04:14 -0400

    added fetch call and hard coded date due to lack of images for current date

--- src/App.js
+++ src/App.js
@@ -11,15 +11,20 @@ function App() {
   useEffect(() => {
     document.title = 'Mars Rover Today: Shopify Frontend Challenge'
 
-    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-9-16&api_key=DEMO_KEY')
-    console.log("today's date is", currentDate)
+    // fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getCurrentDate()}&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-9-16&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
+      .then(resp => resp.json())
+      .then(respJson => {
+        console.log(respJson)
+      })
+      .catch(error => console.log(error))
   }, [])
 
   return (
     <div className="App">
       <NavBar />
       <div className="main">
-
+        {getCurrentDate()}
       </div>
     </div>
   );
