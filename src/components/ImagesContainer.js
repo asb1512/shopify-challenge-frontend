f0500249182902cbe5ea1ab084e714961e5ef305
asb1512 <andrew@olympicmedia.com>
2021-09-17 01:47:08 -0400

    react-bootstrap boilerplate columns in a container

--- src/components/ImagesContainer.js
+++ src/components/ImagesContainer.js
@@ -1,5 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function ImagesContainer() {
   return (
-    <div></div>
+    <Container fluid>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   )
 }
