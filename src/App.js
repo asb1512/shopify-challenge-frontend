6e11b1cedfb62c1c4795f16d3070ab1f10e22589
asb1512 <andrew@olympicmedia.com>
2021-09-17 01:46:32 -0400

    adding local state to track loading status and today's date

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getCurrentDate } from './utils/currentDate';
 import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
+  
+  const [currentDate] = useState(Date())
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     document.title = 'Mars Rover Today: Shopify Frontend Challenge'
+
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-9-16&api_key=DEMO_KEY')
+    console.log("today's date is", currentDate)
   }, [])
 
   return (
