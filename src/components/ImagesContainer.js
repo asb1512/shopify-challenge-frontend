794a46bd4890d11a73143a6809147e0c965aa4eb
asb1512 <andrew@olympicmedia.com>
2021-09-17 02:16:33 -0400

    removed rows and columns to ImageGroup

--- src/components/ImagesContainer.js
+++ src/components/ImagesContainer.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import ImageGroup from './ImageGroup';
 
 export default function ImagesContainer(props) {
+
+  const renderGroups = () => {
+    
+  }
+
   return (
     <Container fluid>
-      <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
+      {props.images ? renderGroups() : null}
     </Container>
   )
 }
