d2f0f5e4dd43c20e2d250380c93f7ab36c32756a
asb1512 <andrew@olympicmedia.com>
2021-09-17 15:26:48 -0400

    taking first fifteen results and would like to lazy load the rest

--- src/components/ImagesContainer.js
+++ src/components/ImagesContainer.js
@@ -4,8 +4,22 @@ import ImageGroup from './ImageGroup';
 
 export default function ImagesContainer(props) {
 
+  // const arrayLength = props.images.length
+  // const remainder = arrayLength % 5
+  // const numOfFullIterations = Math.trunc(arrayLength / 5)
+
+  const firstPiece = props.images?.slice(0, 5)
+  const secondPiece = props.images?.slice(5, 10)
+  const thridPiece = props.images?.slice(10, 15)
+
   const renderGroups = () => {
-    
+    return (
+      <>
+        <ImageGroup images={firstPiece} />
+        <ImageGroup images={secondPiece} />
+        <ImageGroup images={thridPiece} />
+      </>
+    )
   }
 
   return (
