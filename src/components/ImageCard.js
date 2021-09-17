b77fac553c4f8e7812029a6becaec16b8755ada4
asb1512 <andrew@olympicmedia.com>
2021-09-17 15:57:45 -0400

    setting up ability to like an image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,9 +1,19 @@
+import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 export default function ImageCard(props) {
 
   console.log("Image card data", props.image)
+
+  const [likes, incrementLikes] = useState(0)
+  const [likeText, setLikeText] = useState('likes')
+
+  const updateLikes = () => {
+    if (likes + 1 === 1) {
+
+    }
+  }
 
   return (
     <Card style={{ width: '90%', height: '100%' }}>
@@ -17,9 +27,13 @@ export default function ImageCard(props) {
           Camera Type: <br/> {props.image.camera.full_name}
         </Card.Text>
         <div className="d-grid">
-        <Button variant="primary" size="lg">
-          Like
-        </Button>
+          <Button variant="outline-danger" style={{marginBottom: '1rem'}} disabled>
+            {likes} {likeText}
+          </Button>{' '}
+
+          <Button variant="danger" size="lg" onClick={() => updateLikes()}>
+            Like
+          </Button>
         </div>
       </Card.Body>
     </Card>
