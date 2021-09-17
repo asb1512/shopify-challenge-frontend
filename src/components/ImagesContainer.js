ffd81e7658487a05d5c449c806357fb40b681b91
asb1512 <andrew@olympicmedia.com>
2021-09-17 16:34:49 -0400

    removed unnecessary import and useState hooks

--- src/components/ImagesContainer.js
+++ src/components/ImagesContainer.js
@@ -1,12 +1,7 @@
-import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import ImageGroup from './ImageGroup';
 
 export default function ImagesContainer(props) {
-
-  // const arrayLength = props.images.length
-  // const remainder = arrayLength % 5
-  // const numOfFullIterations = Math.trunc(arrayLength / 5)
 
   const firstPiece = props.images?.slice(0, 5)
   const secondPiece = props.images?.slice(5, 10)
