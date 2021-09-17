9940dd28b8426b1e045db4bacd3355260196b270
asb1512 <andrew@olympicmedia.com>
2021-09-17 01:26:54 -0400

    using useEffect hook to set App's title attribute

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useEffect } from 'react';
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Mars Rover Today: Shopify Frontend Challenge'
+  }, [])
+
   return (
     <div className="App">
       <NavBar />
+      <div className="main">
+
+      </div>
     </div>
   );
 }
