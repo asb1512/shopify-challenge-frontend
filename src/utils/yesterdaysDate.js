7e1323b94f32a12551fa3d2cc9e98ef7557c3e45
asb1512 <andrew@olympicmedia.com>
2021-09-18 01:55:26 -0400

    created func that calculates yesterday's date from currentDate func

---
+++ src/utils/yesterdaysDate.js
@@ -0,0 +1,15 @@
+import { getCurrentDate } from "./currentDate";
+
+export function getYesterdaysDate() {
+  const splitStrArray = getCurrentDate().split('-')
+  const yyyy = splitStrArray[0]
+  const mm = splitStrArray[1]
+  const dd = () => {
+    const int = parseInt(splitStrArray[2], 10) - 1
+    const str = int.toString()
+    return str
+  }
+  return `${yyyy}-${mm}-${dd()}`
+}
+
+console.log("yesterday's date", getYesterdaysDate())
