2c0bfc5e33665beb6f8b2ca8c19e421d0546ee3e
asb1512 <andrew@olympicmedia.com>
2021-09-18 03:45:24 -0400

    trying multiple approaches to solve this issue

--- src/components/ImagesContainer.js
+++ src/components/ImagesContainer.js
@@ -8,31 +8,32 @@ export default function ImagesContainer(props) {
   const secondPiece = props.images?.slice(5, 10)
   const thridPiece = props.images?.slice(10, 15)
 
-  const [firstGroup, setFirstGroup] = useState([])
-  const [secondGroup, setSecondGroup] = useState([])
-  const [threeGroup, setThreeGroup] = useState([])
+  const [firstGroup, setFirstGroup] = useState([null, null, null, null, null])
+  const [secondGroup, setSecondGroup] = useState({0: null, 1: null, 2: null, 3: null, 4: null,})
+  const [threeGroup, setThreeGroup] = useState({0: null, 1: null, 2: null, 3: null, 4: null,})
 
   const randomizeGroups = () => {
     let length = props.images.length - 1
-    for (let i = 0; i < 4; i++) {
-      const min = Math.ceil(0)
-      const max = Math.floor(length)
-      const int = (Math.random() * (max - min) + min)
-      setFirstGroup(firstGroup.push(int))
-    }
-    for (let i = 0; i < 4; i++) {
-      const min = Math.ceil(0)
-      const max = Math.floor(length)
-      const int = (Math.random() * (max - min) + min)
-      setSecondGroup(secondGroup.push(int))
-    }
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
       const min = Math.ceil(0)
       const max = Math.floor(length)
       const float = (Math.random() * (max - min) + min)
       const int = Math.trunc(float)
-      setThreeGroup(threeGroup.push(int))
+      setFirstGroup(...firstGroup, firstGroup[i] = int,)
     }
+    // for (let i = 0; i < 4; i++) {
+    //   const min = Math.ceil(0)
+    //   const max = Math.floor(length)
+    //   const int = (Math.random() * (max - min) + min)
+    //   setSecondGroup(secondGroup.push(int))
+    // }
+    // for (let i = 0; i < 4; i++) {
+    //   const min = Math.ceil(0)
+    //   const max = Math.floor(length)
+    //   const float = (Math.random() * (max - min) + min)
+    //   const int = Math.trunc(float)
+    //   setThreeGroup(threeGroup.push(int))
+    // }
   }
   randomizeGroups()
   console.log("first group", firstGroup)
