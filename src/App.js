20be89d15793b65e82db2f0b7aadea67a32c040c
asb1512 <andrew@olympicmedia.com>
2021-09-17 22:33:57 -0400

    changed fetch request to use current date and passed loading status to ImageContainer

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ function App() {
   useEffect(() => {
     document.title = 'Mars Rover Today: Shopify Frontend Challenge'
 
-    // fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getCurrentDate()}&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-9-16&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getCurrentDate()}&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
       .then(resp => resp.json())
       .then(respJson => {
         console.log(respJson)
@@ -26,7 +25,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ImagesContainer images={images} />
+      <ImagesContainer images={images} isLoading={isLoading} />
     </div>
   );
 }
