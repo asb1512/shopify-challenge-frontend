7cb2145e94536301b4264d419bcc23859ed1ba8f
asb1512 <andrew@olympicmedia.com>
2021-09-21 00:46:33 -0400

    split today and yesterday's images into two distinct components to better work with react-router

--- src/components/ImagesContainerOther.js
+++ src/components/ImagesContainerOther.js
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
-import { getCurrentDate } from '../utils/currentDate';
+import { getYesterdaysDate } from '../utils/yesterdaysDate';
 import Container from 'react-bootstrap/Container';
 import ImageGroup from './ImageGroup';
 import Spinner from 'react-bootstrap/Spinner';
-import Button from 'react-bootstrap/Button';
 
-export default function ImagesContainer() {
+
+export default function ImagesContainerOther() {
 
   const [isLoading, setLoading] = useState(true)
   const [images, setImages] = useState(null)
 
   useEffect(() => {
-    fetchToday()
+    fetchYesterday()
   }, [])
 
-  const fetchToday = () => {
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getCurrentDate()}&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
+  const fetchYesterday = () => {
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getYesterdaysDate()}&api_key=TxaYe4m8QQn29wWfry8ck714cekGTjQ3SAF78rQD`)
       .then(resp => resp.json())
       .then(respJson => {
         console.log(respJson)
@@ -30,12 +29,6 @@ export default function ImagesContainer() {
   const secondPiece = images?.slice(5, 10)
   const thridPiece = images?.slice(10, 15)
 
-  const renderRedirect = () => {
-    return (
-      <Redirect to="yesterday" />
-    )
-  }
-
   const renderContent = () => {
     if (isLoading) {
       return (
@@ -45,21 +38,11 @@ export default function ImagesContainer() {
           <Spinner animation="grow" className="m-10-100" />
         </div>
       )
-    } else if (images.length === 0) {
-      return (
-        <div className="too-early">
-          <div className="sad-face">:-(</div>
-          <div className="">There aren't any pictures yet today...</div>
-          <Button variant="outline-danger" className="button" onClick={() => renderRedirect()}>
-            View yesterday's pics
-          </Button>
-        </div>
-      )
     } else {
       return (
         <div className="fullscreen">
           <Container fluid>
-            <ImageGroup images={firstPiece} isLoading={isLoading} />       
+            <ImageGroup images={firstPiece} isLoading={isLoading} />
             <ImageGroup images={secondPiece} isLoading={isLoading} />
             <ImageGroup images={thridPiece} isLoading={isLoading} />
           </Container>
