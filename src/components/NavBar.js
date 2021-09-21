e29dda87d63c1167451e84aecb563a6a1ddb2f45
asb1512 <andrew@olympicmedia.com>
2021-09-20 23:58:24 -0400

    linking navbar items

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,24 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Mars Rover Images</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand href="#home">Mars Rover Images</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Today</Nav.Link>
-            <Nav.Link href="#link">Yesterday</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link href="#home">Today</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/yesterday">
+              <Nav.Link href="#link">Yesterday</Nav.Link>
+            </LinkContainer>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
